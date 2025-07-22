4c1b7a773d764cd5c228e67d97c2364f694cb76c
Gaurav <gauravyadav199808@gmail.com>
2025-07-22 11:12:31 +0530

    Added memory to manage chat history

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import os 
 from dotenv import load_dotenv
-from unstructured.partition.pdf import partition_pdf
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 import gradio as gr
+import utils
 
+llm = ChatOllama(model="llama3:latest", temperature=0.2)
+prompt = ChatPromptTemplate.from_messages([
+    ('system' ,  "You are a specialized assistant for answering questions based ONLY on the provided context from PDF documents. Your instructions are to be followed exactly. 1. Review the 'Context' below. 2. If the 'Context' contains the information to answer the 'Question', provide a helpful answer based solely on that context. 3. If the 'Context' does NOT contain the information to answer the 'Question', you MUST respond with the exact phrase: 'I can not process the request'. 4. Do NOT use any of your internal knowledge. Do NOT attempt to answer if the information is not in the 'Context' except for greetings. Context: {context} "),
+    MessagesPlaceholder(variable_name = "history"),
+    ('user' , '{query}')
+])
 
 def process_query(message, history):
-    messages = []
+    context = []
+    
+    for msg in history:
+        if msg['role'] == 'user' :
+            context.append(HumanMessage(content= msg['content']))
+        else :
+            context.append(AIMessage(content = msg['content'] ))
+    
+    chain = prompt | llm
+    if len(message['files']) == 0 :
+        yield "Processing Query"
+    else :
+        if message['text'].strip() == "" :
+            yield "Processing PDF"
+        else :
+            yield "waiting"
     return {"text" : f"You said: {message['text']}"}
 
 def main():
